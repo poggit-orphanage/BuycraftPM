190abb7b87275e0c74488201cfb38df802355431
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-09-14 07:30:16 +0800

    Update DuePlayerCheck.php
    dang..

--- src/Buycraft/PocketMine/Execution/DuePlayerCheck.php
+++ src/Buycraft/PocketMine/Execution/DuePlayerCheck.php
@@ -62,7 +62,7 @@ class DuePlayerCheck extends AsyncTask{
 		));
 	}
 
-	public function onCompletion(Server $server){
+	public function onCompletion(Server $server) : void {
 		$result = $this->getResult();
 		BuycraftPlugin::getInstance()->getLogger()->info("Found " . count($result['all_due']) . " due player(s).");
 		BuycraftPlugin::getInstance()->setAllDue($result['all_due']);
